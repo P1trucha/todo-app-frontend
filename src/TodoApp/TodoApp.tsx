02e6782fc619c5aa326bd230f33048738fc97673
Piotr Mazepka <piotrmazepka@gmail.com>
2022-08-05 15:24:47 +0200

    feate: Create TodoApp

---
+++ src/TodoApp/TodoApp.tsx
@@ -0,0 +1,21 @@
+import react from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+export const TodoApp = () =>{
+<Container fluid>
+<div className="Container-form">
+<Row>
+
+</Row>
+</div>
+
+
+
+</Container>
+
+
+}
+
+
+export default TodoApp;
